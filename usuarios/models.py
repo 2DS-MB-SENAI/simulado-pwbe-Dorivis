cb332df58f74f11f54d1c951f44452420a082a1f
Dorival Alves Silva Junior <sn1090723@educ501.local>
2025-04-03 12:52:59 -0300

    3

--- usuarios/models.py
+++ usuarios/models.py
@@ -3,27 +3,5 @@ from django.db import models
 
 class Usuario(AbstractUser):
     telefone = models.CharField(max_length=15)
-    
-    # Adicione related_name personalizados
-    groups = models.ManyToManyField(
-        Group,
-        verbose_name='groups',
-        blank=True,
-        help_text='The groups this user belongs to.',
-        related_name='usuarios_groups',  # Nome único
-        related_query_name='usuario',
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        verbose_name='user permissions',
-        blank=True,
-        help_text='Specific permissions for this user.',
-        related_name='usuarios_permissions',  # Nome único
-        related_query_name='usuario',
-    )
-
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['telefone']
-
     def __str__(self):
         return self.username
