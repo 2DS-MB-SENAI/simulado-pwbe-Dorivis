72504d5eb97325f2ef6434417182138f21a9c6f5
Dorival Alves Silva Junior <sn1090723@educ501.local>
2025-04-03 11:27:31 -0300

    fisrt

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-_nk+6(3f0b=!ihy^ap*%8oike_g6wy8n#9mr76yd6(o%utqxpy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+if 'test' in sys.argv:
+    ALLOWED_HOSTS = ['*', 'testserver']
+else:
+    ALLOWED_HOSTS = ['seu-dominio.com']  # Seu host em produção
 
 
 # Application definition
--- livros/urls.py
+++ livros/urls.py
@@ -1,7 +1,6 @@
-# livros/urls.py
 from django.urls import path
-from .views import listar_livros
+from .views import listar_livros  # Importe a view corretamente
 
 urlpatterns = [
-    path('livros/', listar_livros),
+    path('livros/', listar_livros, name='listar_livros'),
 ]
