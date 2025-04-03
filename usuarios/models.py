f0d02056eb265b2f3856ab4a5bc6926e1aa031c0
Dorival Alves Silva Junior <sn1090723@educ501.local>
2025-04-03 12:45:41 -0300

    3

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,10 +1,27 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class Usuario(AbstractUser):
     telefone = models.CharField(max_length=15)
     
-    # Campos adicionais para facilitar a autenticação
+    # Adicione related_name personalizados
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        related_name='usuarios_groups',  # Nome único
+        related_query_name='usuario',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name='usuarios_permissions',  # Nome único
+        related_query_name='usuario',
+    )
+
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['telefone']
 
