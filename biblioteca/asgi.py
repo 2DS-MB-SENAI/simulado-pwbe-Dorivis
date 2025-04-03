708043fe0b986708fc7e2830e65d98724ffd3316
Dorival Alves Silva Junior <sn1090723@educ501.local>
2025-04-03 08:55:25 -0300

    fisrt

--- biblioteca/asgi.py
+++ biblioteca/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for simulado_PWBE project.
+ASGI config for biblioteca project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simulado_PWBE.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblioteca.settings')
 
 application = get_asgi_application()
