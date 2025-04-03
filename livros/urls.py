029a1c8473eb43a1fa3acfcb7ada22fad1867cbd
Dorival Alves Silva Junior <sn1090723@educ501.local>
2025-04-03 12:26:49 -0300

    2

--- livros/urls.py
+++ livros/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import listar_livros  # Importe a view corretamente
+from .views import listar_livros, livros_api  # Importe a view corretamente
 
 urlpatterns = [
     path('livros/', listar_livros, name='listar_livros'),
+    path('api/livros/', livros_api, name='livros-api'),
 ]
--- livros/views.py
+++ livros/views.py
@@ -1,7 +1,25 @@
 # livros/views.py
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Livro
+from .serializers import LivroSerializer
 
 def listar_livros(request):
     livros = Livro.objects.all()
     return render(request, 'livros.html', {'livros': livros})
+
+
+@api_view(['GET', 'POST'])
+def livros_api(request):
+    if request.method == 'GET':
+        livros = Livro.objects.all()
+        serializer = LivroSerializer(livros, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = LivroSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
